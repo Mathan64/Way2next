efe8fed5e9347470112b3a12d85bca8b4a5e5685
mathan64 <karuppanmadhan@gmail.com>
2024-03-24 12:50:52 +0530

    project1

--- src/App.js
+++ src/App.js
@@ -3,30 +3,27 @@ import React, { useState, useEffect } from 'react';
 import Nav from "./components/nav/Nav";
 import Card from "./components/card/Card";
 import Mpage from "./components/mpage/Mpage";
-
 const App = () => {
   const API_URL = 'https://way2next.onrender.com/api/v1/college'||[];
   const [itemsList, setItemsList] = useState([]);
   const [searchInput, setSearchInput] = useState('');
-
   useEffect(() => {
     const fetchItems = async () => {
       try {
         const response = await fetch(API_URL);
         const listItems = await response.json();
-        setItemsList(listItems);
+        setItemsList(listItems.content);
       } catch (err) {
         console.log(err.stack);
       }
     };
-
     fetchItems();
   }, []);
-
+  const filteredItems = Array.isArray(itemsList) ? itemsList.filter(item => item.collegeName.toLowerCase().includes(searchInput.toLowerCase())) : [];
   return (
     <div>
       <Nav searchInput={searchInput} setSearchInput={setSearchInput} />
-      <Card itemsList={itemsList.filter(item => item.collegeName.toLowerCase().includes(searchInput.toLowerCase()))} />
+      <Card itemsList={filteredItems} />
       <Mpage />
     </div>
   );
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -7,17 +7,18 @@ const Card = ({ itemsList }) => {
   return (
     <main>
       {itemsList.map((item) => (
+        <div className='cards'>
         <div className="card" key={item.id}>
-          <img className="cimg" src={item.imgUrl} alt='college' height="50px" width="50px" />
-          <div className='inercard'>
-            <div className='lab'>
-              <label className='cardcollege'>{item.collegeName}<span className='collegecode'>#{item.id}</span></label>
+          <div className='element'>
+            <img className="cimg" src={item.imgUrl} alt='.'/>
+            </div> <div className='element'>
+              <label className='cardcollege'>{item.collegeName}</label> </div>
+              <div className='element'><span className='collegecode'>#{item.id}</span></div>
+          <div className='element'> <p className='cardloc'><IoLocation />{item.location}</p>
+             </div>
             </div>
-            <div>
-              <p className='cardloc'><IoLocation />{item.location}</p>
             </div>
-          </div>
-        </div>
+        
       ))}
     </main>
   );
